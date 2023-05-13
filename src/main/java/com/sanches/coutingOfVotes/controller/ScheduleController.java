57f012d550a53ebed816de2034059e7280dd71a6
marcosvinicius <marcossanches710@gmail.com>
2023-05-13 18:53:19 -0300

    Criando a classe ScheduleController.java para iniciar os testes e cadastros no banco de dados

---
+++ src/main/java/com/sanches/coutingOfVotes/controller/ScheduleController.java
@@ -0,0 +1,38 @@
+package com.sanches.coutingOfVotes.controller;
+
+import com.sanches.coutingOfVotes.controller.request.ScheduleRequest;
+import com.sanches.coutingOfVotes.controller.response.ScheduleResponse;
+import com.sanches.coutingOfVotes.entity.ScheduleEntity;
+import com.sanches.coutingOfVotes.exception.BadRequestException;
+import com.sanches.coutingOfVotes.service.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequestMapping("v1/api/schedule")
+@RestController
+public class ScheduleController {
+
+    private ScheduleService scheduleService;
+
+    @Autowired
+    public ScheduleController(ScheduleService scheduleService){
+        this.scheduleService = scheduleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registerNewSchedule(
+            @Valid @RequestBody ScheduleRequest request)throws BadRequestException {
+        ScheduleResponse response = this.scheduleService.newSchedule(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("list/all")
+    public List<ScheduleEntity> getAllSchedules(){
+        return scheduleService.listAllSchedules();
+    }
+}
