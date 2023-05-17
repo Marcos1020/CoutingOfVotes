28e55610b493e4be1e558a9a93ca2dcd8669936b
marcosvinicius <marcossanches710@gmail.com>
2023-05-17 11:18:50 -0300

    Adicionando o @ComponenteScan para fazer o mapeamento correto do repositório

--- src/main/java/com/sanches/coutingOfVotes/repository/ScheduleRepository.java
+++ src/main/java/com/sanches/coutingOfVotes/repository/ScheduleRepository.java
@@ -1,13 +1,15 @@
 package com.sanches.coutingOfVotes.repository;
 
 import com.sanches.coutingOfVotes.entity.ScheduleEntity;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
+@ComponentScan
+public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long>{
 
-    public Optional <ScheduleEntity> findByScheduleName(final String scheduleName);
+    Optional <ScheduleEntity> findByScheduleName(final String scheduleName);
 }
