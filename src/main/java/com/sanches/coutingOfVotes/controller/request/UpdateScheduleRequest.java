406cf880e8b1d137b69615259d33098a23e3e561
marcosvinicius <marcossanches710@gmail.com>
2023-05-18 11:41:54 -0300

    Criando classe UpdateScheduleRequest.java

---
+++ src/main/java/com/sanches/coutingOfVotes/controller/request/UpdateScheduleRequest.java
@@ -0,0 +1,17 @@
+package com.sanches.coutingOfVotes.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateScheduleRequest {
+
+    private String scheduleName;
+
+    private String description;
+}
