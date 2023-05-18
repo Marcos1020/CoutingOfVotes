74214ee75f11d35be3bba16c60bea5b5e797115d
marcosvinicius <marcossanches710@gmail.com>
2023-05-18 11:41:32 -0300

    Criando classe UpdateAssociateResponse.java

---
+++ src/main/java/com/sanches/coutingOfVotes/controller/response/UpdateAssociateResponse.java
@@ -0,0 +1,32 @@
+package com.sanches.coutingOfVotes.controller.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.sanches.coutingOfVotes.statusenum.StatusAssociate;
+import com.sanches.coutingOfVotes.utils.ConverterUtil;
+import com.sanches.coutingOfVotes.utils.DateAndTimeDeserializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateAssociateResponse {
+
+    private Long idAssociate;
+
+    private String name;
+
+    private String cpf;
+
+    private StatusAssociate statusAssociate;
+
+    @JsonDeserialize(using = DateAndTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = ConverterUtil.FORMATO_DATA)
+    private Date dateUpdate;
+}
