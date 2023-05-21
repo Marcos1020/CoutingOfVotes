75933021f45138900e3b9f1d734ab82dfb44424e
marcosvinicius <marcossanches710@gmail.com>
2023-05-21 19:06:20 -0300

    Alterando os capos onde ainda estavam sendo passados como Schedule

--- src/main/java/com/sanches/coutingOfVotes/service/SubjectService.java
+++ src/main/java/com/sanches/coutingOfVotes/service/SubjectService.java
@@ -1,17 +1,15 @@
 package com.sanches.coutingOfVotes.service;
 
-import com.sanches.coutingOfVotes.controller.request.ScheduleRequest;
+import com.sanches.coutingOfVotes.controller.request.SubjectRequest;
 import com.sanches.coutingOfVotes.controller.request.UpdateScheduleRequest;
-import com.sanches.coutingOfVotes.controller.response.ScheduleResponse;
+import com.sanches.coutingOfVotes.controller.response.SubjectResponse;
 import com.sanches.coutingOfVotes.controller.response.UpdateScheduleResponse;
-import com.sanches.coutingOfVotes.convertions.ScheduleConvertions;
-import com.sanches.coutingOfVotes.entity.AssociateEntity;
-import com.sanches.coutingOfVotes.entity.ScheduleEntity;
+import com.sanches.coutingOfVotes.convertions.SubjectConvertions;
+import com.sanches.coutingOfVotes.entity.SubjectEntity;
 import com.sanches.coutingOfVotes.exception.BadRequestException;
 import com.sanches.coutingOfVotes.exception.ObjectAlreadyExists;
-import com.sanches.coutingOfVotes.repository.ScheduleRepository;
-import com.sanches.coutingOfVotes.statusenum.ScheduleStatus;
-import com.sanches.coutingOfVotes.statusenum.StatusAssociate;
+import com.sanches.coutingOfVotes.repository.SubjectRepository;
+import com.sanches.coutingOfVotes.statusenum.SubjectStatus;
 import com.sanches.coutingOfVotes.utils.ConverterUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,55 +21,55 @@ import java.util.Optional;
 
 @Service
 @Slf4j
-public class ScheduleService {
+public class SubjectService {
 
-    private ScheduleConvertions convertions;
+    private SubjectConvertions convertions;
 
-    private ScheduleRepository scheduleRepository;
+    private SubjectRepository scheduleRepository;
 
     @Autowired
-    public ScheduleService(ScheduleConvertions convertions, ScheduleRepository scheduleRepository) {
+    public SubjectService(SubjectConvertions convertions, SubjectRepository scheduleRepository) {
         this.convertions = convertions;
         this.scheduleRepository = scheduleRepository;
     }
 
-    public ScheduleResponse newSchedule(ScheduleRequest request) throws ObjectAlreadyExists {
+    public SubjectResponse newSchedule(SubjectRequest request) throws ObjectAlreadyExists {
 
         log.info("Inserindo uma nova pauta para ser votada", request.toString());
-        Optional<ScheduleEntity> scheduleEntityentity = this.scheduleRepository.findByScheduleName(request.getScheduleName());
+        Optional<SubjectEntity> scheduleEntityentity = this.scheduleRepository.findBySubject(request.getSubject());
 
         if (scheduleEntityentity.isPresent()) {
             log.info("Pauta com o mesmo nome ja foi inserida, favor verifcar o nome e tente novamente");
             throw new ObjectAlreadyExists("Pauta com o mesmo nome ja foi inserida, favor verifcar o nome e tente novamente");
         }
 
-        ScheduleEntity entity = ScheduleEntity.builder().build();
+        SubjectEntity entity = SubjectEntity.builder().build();
         convertions.converterRequestToEntity(request, entity);
         if (entity.getStatus().equals(null)){
             log.info("Status não pode ser nulo");
             throw new BadRequestException("Status não pode ser nulo");
         }
-        ScheduleEntity entitySave = this.scheduleRepository.save(entity);
+        SubjectEntity entitySave = this.scheduleRepository.save(entity);
         log.info(entitySave.toString());
 
-        ScheduleResponse response = ScheduleResponse.builder().build();
+        SubjectResponse response = SubjectResponse.builder().build();
         convertions.convertEntityTOResponse(entitySave, response);
         return response;
     }
 
-    public List<ScheduleEntity> listAllSchedules() {
+    public List<SubjectEntity> listAllSchedules() {
         return scheduleRepository.findAll();
     }
 
     public UpdateScheduleResponse updateSchedule(final Long idSchedule, UpdateScheduleRequest request) throws BadRequestException {
 
-        ScheduleEntity scheduleEntity = this.scheduleRepository.findById(idSchedule).orElse(null);
+        SubjectEntity scheduleEntity = this.scheduleRepository.findById(idSchedule).orElse(null);
         if (ObjectUtils.isEmpty(scheduleEntity)) {
             log.info("Idt nao encontrado na base de dados, favor verificar o numero inserido e tentar novamente");
             throw new BadRequestException("Idt nao encontrado na base de dados, favor verificar o numero inserido e tentar novamente");
         }
         convertions.UpdateConvertEntityToRequest(request, scheduleEntity);
-        ScheduleEntity entitySave = this.scheduleRepository.save(scheduleEntity);
+        SubjectEntity entitySave = this.scheduleRepository.save(scheduleEntity);
 
         UpdateScheduleResponse response = UpdateScheduleResponse.builder().build();
         convertions.updateConvertEntitySaveToResponse(entitySave, response);
@@ -81,17 +79,17 @@ public class ScheduleService {
     public void inactivatingSchedule(final Long idSchedule)throws BadRequestException{
 
         log.info("Pausando uma pauta");
-        Optional <ScheduleEntity> entitySchedule = this.scheduleRepository.findById(idSchedule);
+        Optional <SubjectEntity> entitySchedule = this.scheduleRepository.findById(idSchedule);
 
         if(!entitySchedule.isPresent()){
             log.info("Idt não encontrado na base de dados");
             throw new BadRequestException("Idt não encontrado na base de dados");
 
-        } else if (entitySchedule.get().getStatus().equals(ScheduleStatus.VOTED)){
+        } else if (entitySchedule.get().getStatus().equals(SubjectStatus.VOTED)){
             log.info("Não foi possivel pausar a pauta, pois ela já consta como votada");
             throw new BadRequestException("Não foi possivel pausar a pauta, pois ela já consta como votada");
         }
-        entitySchedule.get().setStatus(ScheduleStatus.STAND_BY);
+        entitySchedule.get().setStatus(SubjectStatus.STAND_BY);
         entitySchedule.get().setDateUpdate(ConverterUtil.nowTime());
         this.scheduleRepository.save(entitySchedule.get());
     }
